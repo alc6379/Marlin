de81797c5d026078be10ef904ce11d02af0d2623
Alex Cavnar <alc6379@gmail.com>
2021-06-21 05:09:38 -0500

    updated boot and status screens

--- Marlin/_Statusscreen.h
+++ Marlin/_Statusscreen.h
@@ -1,49 +1,28 @@
-/**
- * Marlin 3D Printer Firmware
- * Copyright (C) 2019 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
- *
- * Based on Sprinter and grbl.
- * Copyright (C) 2011 Camiel Gubbels / Erik van der Zalm
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- */
 
 /**
- * Custom Status Screen bitmap
+ * Made with Marlin Bitmap Converter
+ * https://marlinfw.org/tools/u8glib/converter.html
  *
- * Place this file in the root with your configuration files
- * and enable CUSTOM_STATUS_SCREEN_IMAGE in Configuration.h.
- *
- * Use the Marlin Bitmap Converter to make your own:
- * http://marlinfw.org/tools/u8glib/converter.html
+ * This bitmap from the file 'Monster_status_text.bmp'
  */
+#pragma once
 
-//
-// Status Screen Logo bitmap
-//
 #define STATUS_LOGO_Y            8
-#define STATUS_LOGO_WIDTH       39
+#define STATUS_LOGO_WIDTH 48
 
 const unsigned char status_logo_bmp[] PROGMEM = {
-  B11111000,B00000001,B10000000,B00000000,B00001100,
-  B01001000,B00000000,B10000000,B00000000,B00010010,
-  B01000011,B11000011,B10001100,B11010000,B00000010,
-  B01110001,B00100100,B10010010,B01100111,B11001100,
-  B01000001,B00100100,B10011110,B01000000,B00000010,
-  B01001001,B00100100,B10010000,B01000000,B00010010,
-  B11111011,B10110011,B11001110,B11100000,B00001100
+  B11111111,B11111111,B11111111,B11111111,B11111111,B11111111,
+  B11011111,B10111111,B11111111,B11111111,B11111111,B11111111,
+  B11001111,B00111111,B11111111,B11111111,B01111111,B11111111,
+  B11001111,B00111111,B11111111,B11111111,B01111111,B11111111,
+  B11010110,B10110000,B11100001,B11100010,B00110001,B11000111,
+  B11010110,B10100110,B01101110,B11011101,B01101110,B11001111,
+  B11010110,B10101111,B01101110,B11011111,B01100000,B11011111,
+  B11011001,B10101111,B01101110,B11100011,B01101111,B11011111,
+  B11011001,B10100110,B01101110,B11011101,B01101110,B11011111,
+  B11011001,B10110000,B11101110,B11100011,B00110001,B11011111,
+  B11111111,B11111111,B11111111,B11111111,B11111111,B11111111,
+  B11111111,B11111111,B11111111,B11111111,B11111111,B11111111
 };
 
 //
