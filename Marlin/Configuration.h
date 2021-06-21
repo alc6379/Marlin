f2d2c05c18222198a077be1efbdad198872296a9
Alex Cavnar <alc6379@gmail.com>
2021-06-21 05:14:15 -0500

    set bed size to reflect new extruder

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1232,8 +1232,8 @@
 // @section machine
 
 // The size of the print bed
-#define X_BED_SIZE 225
-#define Y_BED_SIZE 202
+#define X_BED_SIZE 229
+#define Y_BED_SIZE 217
 
 // Travel limits (mm) after homing, corresponding to endstop positions.
 #define X_MIN_POS 0
